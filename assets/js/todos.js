8287112e417a2a7af14e3dfc917922c7cf4be168
Yanbin Deng <dyb0214@gmail.com>
2018-12-26 23:03:41 -0800

    finish adding and removing todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,19 +1,48 @@
 
+// JQuery only allow to add a listener to the elements that exist when page load 
+// instead, the listener can be added to the element that is for sure on the page 
+// and then specify the element in that element for triggering event 
 //check off Todos by clicking 
-const black = "rgb(0, 0, 0)"
 // https://stackoverflow.com/questions/9714498/toggle-div-color-on-click
-$("li").click( function() {
-	if ($(this).css("color") === black) {
-		// $(this).css("color", "grey")
-		$(this).css({
-			'text-decoration': "line-through",
-			"color": "grey"
-		 })
+
+// const black = "rgb(0, 0, 0)"
+// $("ul").on("click", "li", function() {
+// 	if ($(this).css("color") === black) {
+// 		// $(this).css("color", "grey")
+// 		$(this).css({
+// 			textDecoration: "line-through",
+// 			color: "gray"
+// 		 });
+// 	}
+// 	else 
+// 		$(this).css({
+// 			'text-decoration': "",
+// 			"color": "black"
+// 		 })
+// });
+
+$("ul").on("click", "li", function() {
+	$(this).toggleClass("completed");
+});
+
+
+$("ul").on("click", "span",function (e) {
+	$(this).parent().fadeOut(500, function () {	// `this` here refers to the <span> 
+		$(this).remove(); // `this` here corresponds to the <li> not the <span> 
+	});
+	e.stopProgagation(); 
+	// since the trash icon is inside the <li>  
+	// if the icon is clicked, it will cause any event listener for its 
+	// parent element to be trigger 
+	// if there is a event listener for the UL and body, the icon click will 
+	// cause the Event Listener for UL to BODY to be triggered 
+}); 
+
+const newTodo = "<li> "
+$("input[type='text'").keypress(function(e) {
+	if (e.which === 13) {
+		const todoList = $(this).val();
+		$(this).val("")  //clear the input field  
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoList + "</li>"); //create an new li and added to the todolist ul
 	}
-	else 
-		// $(this).css("color", "black")
-		$(this).css({
-			'text-decoration': "",
-			"color": "black"
-		 })
 });
